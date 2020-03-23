8803cd21cb1f76fdb9f0e31911e7bb054de6f56a
László György Szilas <laci.szilas@gmail.com>
2020-03-23 18:54:45 +0100

    nn model implemented
    first nn model created, necessary packages includedin my-kaggle environment, model file included

--- kaggle-housing/main.py
+++ kaggle-housing/main.py
@@ -3,11 +3,13 @@ import preprocessing
 import decision_tree
 import random_tree
 import extreme_grad_boost
+import neural_network
 
-pp = preprocessing.PreProcessing()
+pp = preprocessing.PreProcessing(force=True)
 X, X_test = pp.get_preprocessed_data()
 
-OLS.predict(X, X_test)
-decision_tree.predict(X, X_test)
-random_tree.predict(X, X_test)
-extreme_grad_boost.predict(X, X_test)
+# OLS.predict(X, X_test)
+# decision_tree.predict(X, X_test)
+# random_tree.predict(X, X_test)
+# extreme_grad_boost.predict(X, X_test)
+neural_network.predict(X, X_test)
---
+++ kaggle-housing/neural_network.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import Dense
+
+random_state = 237
+
+
+def predict(x, x_test):
+    # cut off target variable and id from the data
+    y = pd.DataFrame(x['SalePrice'], columns=['SalePrice'])
+    x.drop(['SalePrice'], axis=1, inplace=True)
+
+    test_ids = x_test.Id
+    x_test.drop(['Id'], axis=1, inplace=True)
+
+    model = Sequential()
+
+    # the input layer
+    model.add(Dense(128, kernel_initializer='normal', input_dim=x.shape[1], activation='relu'))
+
+    # the hidden layers
+    model.add(Dense(256, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(256, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(256, kernel_initializer='normal', activation='relu'))
+
+    # the output layer
+    model.add(Dense(1, kernel_initializer='normal', activation='linear'))
+
+    # compile the network
+    model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+    print(model.summary())
+
+    # create checkpoints
+    checkpoint_name = 'Best-model.hdf5'
+    checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+    callbacks_list = [checkpoint]
+
+    # fit the model
+    # model.fit(x, y, epochs=500, batch_size=32, validation_split=0.2, callbacks=callbacks_list)
+
+    # load the best weight
+    model.load_weights(checkpoint_name)
+    model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+
+    predictions = pd.DataFrame(model.predict(x_test.to_numpy()), columns=['logSalePrice'])
+
+    test_pred = pd.DataFrame({'Id': test_ids, 'SalePrice': np.exp(predictions.logSalePrice)})
+    test_pred.to_csv('submission.csv', index=False)
