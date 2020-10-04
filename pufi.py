0322cb515c679b5e96c89f474dbf3a98b53ec755
KataBenedek <kata.benedek.2018@uni.strath.ac.uk>
2020-10-04 12:59:46 +0100

    Pufi

--- pufi.py
+++ pufi.py
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 # miau miau hol a tasaskosom???
 
 for _ in range(100):
     print('Cuki Pufika')
 
 print("Még több Pufi!")
+=======
+# miau miau hol a tasaskosom???
+>>>>>>> parent of ebf0b38... Pufi cuki
+
+#Pufika
